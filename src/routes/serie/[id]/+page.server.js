3f22afd0784adfb38a0b60e1477c17fe6b7c1ab8
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2023-06-03 20:46:31 -0300

    added seria detail page

--- src/routes/serie/[id]/+page.server.js
+++ src/routes/serie/[id]/+page.server.js
@@ -1,30 +1,20 @@
 import { APIKEY } from '$env/static/private';
 /** @type {import('./$types').PageLoad} */
 
-export async function load({ url }) {
-
-    const path = url.pathname
-    const regex = new RegExp(/[^0-9]/gi);
-    const id = path.replace(regex, '');
+export async function load({ params }) {
+    const id = params.id;
     const serieDetail = await getSerieDetail(id)
     return { serieDetail: serieDetail }
 }
-// console.log(serieDetail)
-// const res = await getMovies();
-// return { series: res,
-//          prueba: "Listado de series" }
-
 
 async function getSerieDetail(serieId) {
     return new Promise(async (resolve, reject) => {
-        const url = `https://api.themoviedb.org/3/tv/${serieId}?api_key=${APIKEY}&language=es-ES&page=1`;
-        console.log(url)
+        const url = `https://api.themoviedb.org/3/tv/${serieId}?api_key=${APIKEY}&language=en-USS&page=1`;
         try {
             const res = await fetch(url);
             const data = await res.json()
             resolve(data)
         } catch (err) {
-            console.log(err)
             reject(err)
         }
     });
--- src/routes/serie/[id]/+page.svelte
+++ src/routes/serie/[id]/+page.svelte
@@ -1,1 +1,46 @@
-<h1>Welcome to detail serie page</h1>
+<script>
+  export let data;
+</script>
+
+<div class="serie-datail">
+    <div class="img-container">
+        <img
+        src={`https://image.tmdb.org/t/p/original${data.serieDetail.poster_path}`}
+        alt={data.serieDetail.name}/>
+    </div>
+    <div class="text">
+        <h1>{data.serieDetail.name}</h1>
+        <h4>Temporadas {data.serieDetail.number_of_seasons}</h4>
+        <p><span>
+            En emisión: {data.serieDetail.in_production}
+        </span></p>
+        <p>{data.serieDetail.overview}</p>
+    </div>
+</div>
+
+<style>
+    h1{
+        padding: 1rem 0rem 2rem;
+    }
+    p{
+        padding: 1rem 0rem;
+    }
+    .img-container{
+        width: 40%;
+        
+    }
+    img{
+        width: 100%;
+        border-radius: 1rem;
+    }
+    .text{
+        float: left
+    }
+    .serie-datail{
+        display: inline-block;
+        margin: 2rem 20%;
+    }
+    span{
+        font-weight: bold;
+    }
+</style>

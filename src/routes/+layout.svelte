13cd83a1574f88d9add5948b791db1ce90e6f602
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2023-06-07 22:18:55 -0300

    added routing and hover efects in navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,80 @@
 <script>
-    import "../app.css";
-    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
+  import "../app.css";
+  import {
+    Navbar,
+    NavBrand,
+    NavLi,
+    NavUl,
+    NavHamburger,
+  } from "flowbite-svelte";
+
+  let home = true;
+  let picker = false;
+  let history = false;
+
+  function setNavBarItemsFalse() {
+    home = false;
+    picker = false;
+    history = false;
+  }
+
+  function handleNavBarItems(page) {
+    setNavBarItemsFalse();
+    if (page === "home") {
+      home = true;
+      return;
+    }
+    if (page === "picker") {
+      picker = true;
+      return;
+    }
+    if (page === "history") {
+      history = true;
+      return;
+    }
+  }
 </script>
+
+<!-- Main navigation container -->
+<Navbar let:hidden let:toggle>
+  <NavBrand href="/">
+    <span
+      class="self-center whitespace-nowrap text-xl font-semibold dark:text-white zoom-in-hover"
+    >
+      Que mierda ver 🤔
+    </span>
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+  <NavUl {hidden}>
+    <NavLi href="/" active={home} on:click={() => handleNavBarItems(home)}>
+      <div class="zoom-in-hover">Home</div>
+    </NavLi>
+    <NavLi
+      href="/picker"
+      class="zoom-in-hover"
+      active={picker}
+      on:click={() => handleNavBarItems("picker")}
+    >
+      <div class="zoom-in-hover">Picker</div>
+    </NavLi>
+    <NavLi
+      href="/history"
+      class="zoom-in-hover"
+      active={history}
+      on:click={() => handleNavBarItems("history")}
+    >
+      <div class="zoom-in-hover">History</div>
+    </NavLi>
+  </NavUl>
+</Navbar>
+
+<slot />
 
 <style>
   .zoom-in-hover {
     transition: transform 0.3s ease-in-out;
   }
-
   .zoom-in-hover:hover {
     transform: scale(1.2);
   }
 </style>
-
-<!-- Main navigation container -->
-<Navbar let:hidden let:toggle>
-    <NavBrand href="/">
-      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white zoom-in-hover">
-        Que mierda ver 🤔 
-      </span>
-    </NavBrand>
-    <NavHamburger on:click={toggle} />
-    <NavUl {hidden}>
-      <NavLi href="/" active={true}>Home</NavLi>
-      <NavLi href="/about">About</NavLi>
-      <NavLi href="/services">Services</NavLi>
-      <NavLi href="/pricing">Pricing</NavLi>
-      <NavLi href="/contact">Contact</NavLi>
-    </NavUl>
-</Navbar>
-
-<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,21 @@
 <script>
   import Series from "../components/Series.svelte";
+  import { Heading, P, Span } from "flowbite-svelte";
   export let data;
 </script>
 
-<!-- <h1>{data.prueba}</h1> -->
+<div class="header">
+  <Heading tag="h3" class="mb-4"><Span gradient>Trending</Span>🔥</Heading>
+</div>
 <section>
-    <Series series={data.series}/>
+  <Series series={data.series} />
 </section>
 
 <style>
   :global(body) {
-		background-color: #101727;
+    background-color: #101727;
+  }
+  .header{
+    padding-left: 5%;
   }
 </style>
